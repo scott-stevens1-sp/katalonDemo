b582f0e14c2a3c2f21a4a3e294782e3605f82dea
stevensscott29 <stevensscott29@gmail.com>
2020-03-30 19:39:50 -0400

    First Commit to GitHib

---
+++ Object Repository/GitHubTest/Test Test Object.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Test Test Object</name>
+   <tag></tag>
+   <elementGuidId>34a8c516-063e-4305-aae1-5dde909b757d</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//input</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
